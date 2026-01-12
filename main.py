6af9538db5325b1434a6f86817c7673c26b23d32
mwangisam203 <mwangisam203@gmail.com>
2026-01-12 02:06:07 -0500

    mod

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 #Python sets
 myset = {"shoes", "Trousers", "Hoodies", "socks"}
 print(myset)
-            #output {"Socks", "Trousers", "Shoes", "Hoodies"}
+            #output: {"Socks", "Trousers", "Shoes", "Hoodies"}
 
 
 #True and 1, False and 0
 myset = {"shoes", "Trousers", 1, True, "Hoodies", 0, "socks", False}
 print(myset)
-            #output {False, 1, 'Hoodies', 'shoes', 'Trousers', 'socks'}
+            #output: {False, 1, 'Hoodies', 'shoes', 'Trousers', 'socks'}
 
 
 #length of a set
 myset = {"shoes", "Trousers", 1, 0, True, "Hoodies", "socks", False}
 print(len(myset))
-            #output 6
+            #output: 6
 
 
 #Duplicates are ignorde
@@ -23,7 +23,7 @@ set3 = {True, 1, 0, False, False}
 print(set1)
 print(set2)
 print(set3)
-            #output {'apple', 'banana', 'cherry'}
+            #output: {'apple', 'banana', 'cherry'}
                    # {1, 3, 5, 7, 9}
                    # {0, True}
 
@@ -31,10 +31,18 @@ print(set3)
 #mixed data types
 mixedset = {"abc", 134, 1, "Goat", True, 45, 0, "male"}
 print(mixedset)
-            # output {'abc', 1, 0, 'male', 'Goat', 134, 45}
+            # output: {'abc', 1, 0, 'male', 'Goat', 134, 45}
 
 
 #set constructor
 myset = set(("shoes", "Trousers", "Hoodies", "socks"))
 print(myset)
-            #output {'shoes', 'Hoodies', 'socks', 'Trousers'}
+            #output: {'shoes', 'Hoodies', 'socks', 'Trousers'}
+
+#Access items
+myset = {"shoes", "Trousers", "Hoodies", "socks"}
+for x in myset:
+    print(x) #output: socks, Trousers, shoes, Hoodies
+
+print("Trousers" in myset)
+            # Output:True
