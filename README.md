4eac7280d90e56ca7d750f80e42374a31e6a6543
mwangisam203 <mwangisam203@gmail.com>
2026-01-12 01:26:46 -0500

    main.py test

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ This project contains examples and explanations of Python sets and frozensets.
 
 ## Topics Covered
 - Set creation
+     Sets are written with curly brackets
+     after creating a set, you cannot access its items using indexes since they are unordered
+     
 - Set operations
 - frozenset
 - add vs update
---
+++ main.py
@@ -0,0 +1,16 @@
+#Python sets
+myset = {"shoes", "Trousers", "Hoodies", "socks"}
+print(myset)
+            #output {"Socks", "Trousers", "Shoes", "Hoodies"}
+
+
+#True and 1, False and 0
+myset = {"shoes", "Trousers", 1, True, "Hoodies", 0, "socks", False}
+print(myset)
+            #output {False, 1, 'Hoodies', 'shoes', 'Trousers', 'socks'}
+
+
+#length of a set
+myset = {"shoes", "Trousers", 1, 0, True, "Hoodies", "socks", False}
+print(len(myset))
+            #output 6
